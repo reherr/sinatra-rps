4f5543287cef12d8c5397c282cae53dc374081e7
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-04-24 01:38:49 +0000

    rock path and template

--- app.rb
+++ app.rb
@@ -5,4 +5,18 @@ get("/") do
   erb(:homepage)
 end
 
-get("/")
+get("/rock") do 
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else @outcome = "won"
+  end
+
+erb(:zebra)
+
+end
